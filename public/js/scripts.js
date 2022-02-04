7632a584c42c4910613eac2734ad12d661d4064a
JeongCheolLee <85550653+JeongCheolLee@users.noreply.github.com>
2022-02-04 16:55:28 +0900

    event design and basic logic finished

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -9,12 +9,38 @@ const formElement = getElementById('chat_form');
 
 // global socket handler
 socket.on('user_connected', (username) => {
-  console.log(`${username} connected!`);
+  drawNewChat(`${username} connected!`);
 });
+socket.on('new_chat', (data) => {
+  const { chat, username } = data;
+  drawNewChat(`${username} : ${chat}`);
+});
+
+// event callback functions
+const handleSubmit = (event) => {
+  //event bubble 막기
+  event.preventDefault();
+  const inputValue = event.target.elements[0].value;
+  if (inputValue !== '') {
+    socket.emit('submit_chat', inputValue);
+    // 화면에다가 그리기
+    drawNewChat(`me : ${inputValue}`);
+    event.target.elements[0].value = '';
+  }
+};
 
 //* draw functions
 const drawHelloStrager = (username) => {
   helloStrangerElement.innerText = `Hello ${username} Stranger :)`;
+};
+const drawNewChat = (message) => {
+  const wrapperChatBox = document.createElement('div');
+  const chatBox = `
+        <div>
+        ${message}
+        </div>`;
+  wrapperChatBox.innerHTML = chatBox;
+  chattingBoxElement.append(wrapperChatBox);
 };
 
 function helloUser() {
@@ -26,6 +52,8 @@ function helloUser() {
 
 function init() {
   helloUser();
+  // 이벤트 연결
+  formElement.addEventListener('submit', handleSubmit);
 }
 
 init();
