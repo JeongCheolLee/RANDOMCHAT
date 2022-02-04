a89c5fa1b1adb9ded422fa3a1e9d6e8f0a4addc6
JeongCheolLee <85550653+JeongCheolLee@users.noreply.github.com>
2022-02-04 13:49:11 +0900

    life cycle of gateway test finished

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -4,10 +4,36 @@ import {
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
 
-@WebSocketGateway()
-export class ChatsGateway {
+@WebSocketGateway({ namespace: 'chattings' })
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger('chat');
+
+  constructor() {
+    this.logger.log('constructor');
+  }
+
+  //연결해제순간 실행
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`disconnected : ${socket.id} ${socket.nsp.name}`);
+  }
+
+  //연결되는순간 실행
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`connected : ${socket.id} ${socket.nsp.name}`);
+  }
+
+  afterInit() {
+    this.logger.log('init');
+  }
+
   @SubscribeMessage('new_user')
   handleNewUser(
     @MessageBody() username: string,
