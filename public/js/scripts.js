158f9b98b73e2f1a7d85b8c97f63a353ede91b26
JeongCheolLee <85550653+JeongCheolLee@users.noreply.github.com>
2022-02-04 13:20:44 +0900

    client & server setting finished

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,19 @@
-alert('hello world');
+const socket = io('/');
+
+const getElementById = (id) => document.getElementById(id) || null;
+
+// get Dom element
+
+const helloStrangerElement = getElementById('hello_stranger');
+const chattingBoxElement = getElementById('chatting_box');
+const formElement = getElementById('chat_form');
+
+function helloUser() {
+  const username = propmt('What is your name?');
+}
+
+function init() {
+  helloUser();
+}
+
+init();
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { ChatsModule } from './chats/chats.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { Module, NestModule } from '@nestjs/common';
@@ -13,6 +14,7 @@ import * as mongoose from 'mongoose';
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }),
+    ChatsModule,
   ],
   controllers: [AppController],
   providers: [],
